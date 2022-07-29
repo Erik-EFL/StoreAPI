7f13feffaebfd23eb3998ccbe8833b17d0551b03
Erik-EFL <erik.lima.f@outlook.com>
2022-07-29 20:13:47 -0300

    feat(Model): :sparkles: Adicionando a function post.create a camada module
    Creando a função necessaria para criar um novo produto na camada de model

---
+++ src/models/products.model.ts
@@ -0,0 +1,22 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { DataProduct } from '../interfaces/product.interface';
+import connection from './connection';
+import Query from './db.query';
+
+class ProductsModel {
+  public connection: Pool;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  public create = async (product: DataProduct) => {
+    const [newProduct] = await this.connection.query<ResultSetHeader>(
+      Query.products.create,
+      [product.name, product.amount],
+    );
+    return { id: newProduct.insertId, ...product };
+  };
+}
+
+export default ProductsModel;
